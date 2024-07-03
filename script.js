c98473c0c0346716addfdba842fceb5e0cdb5a77
Ajithsingh Athmasaran Sajitha <s.ajithsingh@noxum.com>
2024-07-03 10:11:45 +0530

    made create playlist button pointer and removed few song which does not have images

--- script.js
+++ script.js
@@ -1,49 +1,5 @@
 const songs = [
   {
-    id: 1,
-    name: "Song Title 1",
-    artist: "Artist Name 1",
-    img: "./music/horse.ogv",
-    genre: "Genre 1",
-    source: "./music/horse.ogv",
-  },
-  {
-    id: 2,
-    name: "Song Title 2",
-    artist: "Artist Name 2",
-    img: "img_url_2.jpg",
-    genre: "Genre 2",
-    source:
-      "https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg",
-  },
-  {
-    id: 3,
-    name: "Song Title 3",
-    artist: "Artist Name 3",
-    img: "img_url_3.jpg",
-    genre: "Genre 3",
-    source:
-      "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/theme_01.mp3",
-  },
-  {
-    id: 4,
-    name: "Song Title 4",
-    artist: "Artist Name 4",
-    img: "img_url_4.jpg",
-    genre: "Genre 2",
-    source:
-      "https://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/ateapill.ogg",
-  },
-  {
-    id: 5,
-    name: "Song Title 5",
-    artist: "Artist Name 5",
-    img: "img_url_5.jpg",
-    genre: "Genre 3",
-    source:
-      "https://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3",
-  },
-  {
     id: 6,
     name: "Death Bed",
     artist: "Powfu",
